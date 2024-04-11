a0dd393053683d978e979779009c117d80c45502
aAlbrtx <albertferreres68@gmail.com>
2024-04-11 17:14:14 +0200

    patterns

--- GAMESHOP/src/tiendajuegos/MAIN.java
+++ GAMESHOP/src/tiendajuegos/MAIN.java
@@ -1,11 +1,43 @@
 package tiendajuegos;
 
-public class MAIN {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class MAIN {
+static Scanner lector = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("MENU");
-		System.out.println("HOLA");
-		System.out.println("BUENAS");
+		System.out.println("Bienvenido a JAC'Games'");
+		lector.nextLine();
+		
+		System.out.println("Regístrate para entrar en nuestra web");
+		lector.nextLine();
+		System.out.println("Introduce tu gmail");
+		String gmail = lector.nextLine();
+		
+		
+		Pattern p = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		Matcher m = p.matcher(gmail);
+		if(m.matches()) {
+			System.out.println("Introduce tu contraseña");
+			String contraseña = lector.nextLine();
+			
+			
+			
+			System.out.println("EXCELSIOR!");
+			
+			System.out.println("¿Qué desea hacer?");
+		} else {
+			System.err.println("Ese usuario no existe");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
